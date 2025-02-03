b0afd1df0dd9656048d84b1983bc94602849a18e
Coker <gbolahancoker99@gmail.com>
2025-02-03 17:16:50 +0100

    okay ooo, ooo, ooo, ooo, ooo, o

--- src/Components/Product/ProductDetails/ProductDetailsHero.tsx
+++ src/Components/Product/ProductDetails/ProductDetailsHero.tsx
@@ -1,18 +1,20 @@
-import {useEffect, useState} from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { ProductsDetails } from '../../types/types';
-import { products } from "../../data/ProductDatas"; 
-import ProductDetailsContext from './ProductDetailsContext';
-import ProductVarities from './ProductVarities';
+import Skeleton from '@mui/material/Skeleton';
+import { useEffect, useState } from 'react';
 import { CiHeart } from "react-icons/ci";
 import { MdKeyboardArrowLeft } from "react-icons/md";
+import { useNavigate, useParams } from 'react-router-dom';
+import { products } from "../../data/ProductDatas";
+import { ProductsDetails } from '../../types/types';
+import ProductDetailsContext from './ProductDetailsContext';
+import ProductVarities from './ProductVarities';
+import { MdOutlineFavorite } from "react-icons/md";
 
 const ProductDetailsHero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState<unknown>(null)
     const {id} = useParams()
     const navigate = useNavigate()
-    console.log(id)
+    const [like, setLike] = useState(false)
     const [data, setData] = useState<ProductsDetails>({
         category: '',
         id: 0,
@@ -28,8 +30,9 @@ const ProductDetailsHero = () => {
         space: [],
         description: ''
       });
-    console.log('0000',data)
-    console.log(products)
+      const handleClick = () => {
+        setLike((prev) => !prev )
+      }
     useEffect(() => {
         const product = products.find((product) => product.id === Number(id));
         console.log('899', product);
@@ -45,15 +48,31 @@ const ProductDetailsHero = () => {
         setIsLoading(false)
       }
     }, [ id ]);
-    const LoadingSpinner = () => (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="w-8 h-8 border-4 border-blue-500 rounded-full animate-spin border-t-transparent"></div>
+  //   const LoadingSpinner = () => (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //       <div className="w-8 h-8 border-4 border-blue-500 rounded-full animate-spin border-t-transparent"></div>
+  //     </div>
+  //   );
+  // if(isLoading){
+  //     return( 
+  //         LoadingSpinner()
+  //     )
+  // }
+  if (isLoading) {
+    return (
+      <div className='pt-[120px] w-[85%] mx-auto'>
+        <div className="grid grid-cols-3 gap-4">
+          {[...Array(5)].map((_, index) => (
+            <Skeleton 
+              key={index} 
+              variant="rectangular" 
+              width="100%" 
+              height={300} 
+            />
+          ))}
+        </div>
       </div>
     );
-  if(isLoading){
-      return( 
-          LoadingSpinner()
-      )
   }
   if(isError){
       return(
@@ -73,9 +92,11 @@ const ProductDetailsHero = () => {
             {data?.title}
           </h1>
         </div>
-        <div className="flex items-center p-2 border rounded-lg ">
-            <span className='mr-2 cursor-pointer'>
-              <CiHeart size={24}/>
+        <div className="flex items-center p-2 border rounded-lg " >
+            <span 
+              className='mr-2 cursor-pointer' 
+              onClick={handleClick}>
+             {like? <MdOutlineFavorite size={24} color='text-red-500' /> : <CiHeart size={24}/>} 
             </span>
             <p>Add to Wishlist</p>
         </div>
--- src/Components/Product/ProductDisplay.tsx
+++ src/Components/Product/ProductDisplay.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdOutlineStar } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
 import { ProductDisplayProps } from '../types/types';
-import { useNavigate} from 'react-router-dom';
+import Skeleton from '@mui/material/Skeleton';
 
 const ProductDisplay: React.FC<ProductDisplayProps> = ({products}:any) => {
   const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState<unknown>(null)
+
+  useEffect(() => {
+    try{
+      if( products){
+        setIsLoading(false);
+      }else{
+        throw new Error("couldn't find product check your internet connection")
+      }
+    }catch(error){
+      setIsError(error)
+      setIsLoading(false)
+  }
+},[products]);
+
+if (isLoading) {
+  return (
+    <div className=' w-[85%] mx-auto'>
+      <div className="grid grid-cols-4 gap-4">
+        {[...Array(8)].map((_, index) => (
+          <Skeleton 
+            key={index} 
+            variant="rectangular" 
+            width="100%" 
+            height={300} 
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+if(isError){
+    return(
+        <div className='flex items-center justify-center min-h-screen'>
+            <p>Kindly check your network server</p>
+        </div>
+    )
+}
 
   return (
     <div>
