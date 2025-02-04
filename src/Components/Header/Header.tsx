c9eb01e2b9d89db1ee52b5f6c148ac5937d8809b
Coker <gbolahancoker99@gmail.com>
2025-02-04 10:51:06 +0100

    okay

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,10 +2,10 @@
 import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div className='bg-red-900 w-full z-999 fixed '>
+    <div className='bg-[#2E96E6] w-full opacity-25 fixed '>
         <div className='flex items-center justify-between z-999 text-white py-7 w-[90%] mx-auto'>
             <h1 className=''>ViVACExpress</h1>
-            <div className='space-x-5'>
+            <div className='space-x-6'>
                 <Link to='#'>Sign up</Link>
                 <Link to='#'>Login</Link>
             </div>
